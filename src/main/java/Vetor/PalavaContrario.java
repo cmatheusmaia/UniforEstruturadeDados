96e84f957cee1c19547e398ff4a804b9f6fc5bc3
Matheus Maia <contato@matheusmaia.com>
2016-04-14 21:52:48 -0300

    Exercicios realizados em sala de aula

---
+++ src/main/java/Vetor/PalavaContrario.java
@@ -0,0 +1,21 @@
+package Vetor;
+
+import java.util.Scanner;
+
+public class PalavaContrario {
+	
+	public static void main(String...args){
+		String palavraC = null;
+		Scanner entrada = new Scanner (System.in);
+		
+		System.out.println("Informe a palavra que deseja visualizar de forma contrário");
+		palavraC = entrada.next();
+		palavraC = new StringBuilder(palavraC).reverse().toString();
+		System.out.println ("A palavra contrário é: "+palavraC);
+		
+		Integer tamanho = palavraC.length();
+		
+		System.out.println("O tamanho da palavra é "+tamanho);
+	}
+
+}
---
+++ src/main/java/Vetor/escAucontrario.java
@@ -0,0 +1,16 @@
+package Vetor;
+import java.io.Serializable;
+import java.util.Scanner;
+public class escAucontrario 
+{
+public static void main(String[] args){
+    String frase = "Java é muito interessante";
+    
+    int tam = frase.length();
+    
+    System.out.println("Esta string contem " + 
+        tam + " caracteres");
+    
+    System.exit(0);
+  }
+} 
